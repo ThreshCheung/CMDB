55dd1a742a4d8b5ecf43793a9303091e57afd26f
ThreshCheung <thresh.zhang@ftxgame.local>
2022-04-01 19:13:59 +0800

    2022.04.01.1

--- ftx-ops/src/api/databases/databasesList.js
+++ ftx-ops/src/api/databases/databasesList.js
@@ -6,3 +6,5 @@ export const reqDatabasesInstance = (pageSize, currentPage) => request({ url: `/
 export const reqDatabasesList = (instance_id)=> request({url:`/v1/database_schemas/?instance_id=${instance_id}`,method:'get'})
 
 export const reqTables = (instance_id,schema)=> request({url:`/v1/schema_tables/?instance_id=${instance_id}&schema=${schema}`,method:'get'})
+
+export const reqRollback = (instanceId,strategy,rollbackTime,rollbackList)=>request({url:'/v1/rollback/',method:'post',instanceId,strategy,rollbackTime,rollbackList})
